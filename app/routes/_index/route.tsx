5d22de28290bbb8e10611e5396a86d6d0bbfc2b5
Tetiana Mandziuk <77341404+tetianaman@users.noreply.github.com>
2024-09-18 17:41:53 +0200

    feat: add category links to the home page (#18)

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -28,6 +28,33 @@ export default function HomePage() {
                     <LabelWithArrow>Shop Collections</LabelWithArrow>
                 </CategoryLink>
             </div>
+
+            <div className="rowOfCards">
+                <CategoryLink categorySlug="kitchen" className="linkCard">
+                    <img
+                        className="linkCardBackground"
+                        src="https://static.wixstatic.com/media/c837a6_c05a03f48fbd49e7b5046d1b18c930eb~mv2.jpg/v1/fill/w_547,h_730,q_90/c837a6_c05a03f48fbd49e7b5046d1b18c930eb~mv2.jpg"
+                        alt=""
+                    />
+                    <div className="linkCardTitle">Kitchen</div>
+                </CategoryLink>
+                <CategoryLink categorySlug="bath" className="linkCard">
+                    <img
+                        className="linkCardBackground"
+                        src="https://static.wixstatic.com/media/c837a6_269f35d6ccff4321b7ed1e65c2835c61~mv2.jpg/v1/fill/w_548,h_730,q_90/c837a6_269f35d6ccff4321b7ed1e65c2835c61~mv2.jpg"
+                        alt=""
+                    />
+                    <div className="linkCardTitle">Bath</div>
+                </CategoryLink>
+                <CategoryLink categorySlug="on-the-go" className="linkCard">
+                    <img
+                        className="linkCardBackground"
+                        src="https://static.wixstatic.com/media/c837a6_d38d8d08196d477ba49efff880d5b918~mv2.jpg/v1/fill/w_547,h_730,q_90/c837a6_d38d8d08196d477ba49efff880d5b918~mv2.jpg"
+                        alt=""
+                    />
+                    <div className="linkCardTitle">On the Go</div>
+                </CategoryLink>
+            </div>
         </>
     );
 }
