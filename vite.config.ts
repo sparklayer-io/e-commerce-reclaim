f1910f2db07906aec2b0207caf43b63cd469f6ef
Oleksiy Letushev <31585545+Letushev@users.noreply.github.com>
2024-09-20 16:06:15 +0200

    feat: responsive product page (#32)

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,20 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { netlifyPlugin } from '@netlify/remix-adapter/plugin';
+import path from 'node:path';
 
 export default defineConfig({
-    plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
+    plugins: [
+        remix({
+            ignoredRouteFiles: ['**/*.module.scss'],
+        }),
+        netlifyPlugin(),
+        tsconfigPaths(),
+    ],
+    resolve: {
+        alias: {
+            '@styles': path.resolve(__dirname, './src/styles/'),
+        },
+    },
+    css: { preprocessorOptions: { scss: { api: 'modern' } } },
 });
