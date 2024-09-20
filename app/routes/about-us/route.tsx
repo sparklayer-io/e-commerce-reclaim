1f765f531a89fe9cacca87bfc8e0c195a35ca042
Yurii Nesteruk <124175587+ynesteruk@users.noreply.github.com>
2024-09-20 16:59:13 +0300

    feat: about page (#29)

--- app/routes/about-us/route.tsx
+++ app/routes/about-us/route.tsx
@@ -1,3 +1,22 @@
+import styles from './about-us.module.scss';
+
 export default function AboutUsPage() {
-    return <h1>About us</h1>;
+    return (
+        <div className={styles.root}>
+            <div className={styles.aboutSection}>
+                <h1 className={styles.title}>We are ReClaim</h1>
+                <div className={styles.subtitle}>A women-owned local business</div>
+                <div className={styles.description}>
+                    This is the space to introduce visitors to the business or brand. Briefly
+                    explain who&#39;s behind it, what it does and what makes it unique. Share its
+                    core values and what this site has to offer.
+                </div>
+            </div>
+            <img
+                className={styles.image}
+                src="https://static.wixstatic.com/media/c837a6_825d7dbd2e634114906169b9674b56fa~mv2.jpg"
+                alt=""
+            />
+        </div>
+    );
 }
