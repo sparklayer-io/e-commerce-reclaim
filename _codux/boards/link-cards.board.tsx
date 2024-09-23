132bdfa0537f268eda371ca2a7f8dea7b793c1e2
Alexey Lebedev <alisey@users.noreply.github.com>
2024-09-23 15:37:42 +0300

    feat: boards for text banner and link cards (#64)

---
+++ _codux/boards/link-cards.board.tsx
@@ -0,0 +1,37 @@
+import { createBoard } from '@wixc3/react-board';
+
+export default createBoard({
+    name: 'Link Cards',
+    Board: () => (
+        <div className="rowOfCards">
+            <a className="linkCard" href="about:blank">
+                <img
+                    className="linkCardBackground"
+                    src="https://static.wixstatic.com/media/c837a6_c05a03f48fbd49e7b5046d1b18c930eb~mv2.jpg/v1/fill/w_547,h_730,q_90/c837a6_c05a03f48fbd49e7b5046d1b18c930eb~mv2.jpg"
+                    alt=""
+                />
+                <div className="linkCardTitle">Kitchen</div>
+            </a>
+            <a className="linkCard" href="about:blank">
+                <img
+                    className="linkCardBackground"
+                    src="https://static.wixstatic.com/media/c837a6_269f35d6ccff4321b7ed1e65c2835c61~mv2.jpg/v1/fill/w_548,h_730,q_90/c837a6_269f35d6ccff4321b7ed1e65c2835c61~mv2.jpg"
+                    alt=""
+                />
+                <div className="linkCardTitle">Bath</div>
+            </a>
+            <a className="linkCard" href="about:blank">
+                <img
+                    className="linkCardBackground"
+                    src="https://static.wixstatic.com/media/c837a6_d38d8d08196d477ba49efff880d5b918~mv2.jpg/v1/fill/w_547,h_730,q_90/c837a6_d38d8d08196d477ba49efff880d5b918~mv2.jpg"
+                    alt=""
+                />
+                <div className="linkCardTitle">On the Go</div>
+            </a>
+        </div>
+    ),
+    environmentProps: {
+        windowWidth: 600,
+        windowHeight: 270,
+    },
+});
---
+++ _codux/boards/text-banner.board.tsx
@@ -0,0 +1,23 @@
+import { createBoard } from '@wixc3/react-board';
+import { LabelWithArrow } from '~/components/label-with-arrow/label-with-arrow';
+
+export default createBoard({
+    name: 'Text Banner',
+    Board: () => (
+        <div className="textBannerSection">
+            <div className="textBanner">
+                <div className="textBannerSubtitle">Products of the highest standards</div>
+                <div className="textBannerTitle">
+                    Essential home collections for sustainable living
+                </div>
+                <a href="about:blank">
+                    <LabelWithArrow>Shop Collections</LabelWithArrow>
+                </a>
+            </div>
+        </div>
+    ),
+    environmentProps: {
+        windowWidth: 600,
+        windowHeight: 220,
+    },
+});
