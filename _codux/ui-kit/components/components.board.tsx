ce0fee5a8aea0989040342c1e874107af36d9e3b
Alexey Lebedev <alisey@users.noreply.github.com>
2024-09-26 19:32:40 +0300

    feat: tidy up the components ui kit and add variant wrappers (#112)

--- _codux/ui-kit/components/components.board.tsx
+++ _codux/ui-kit/components/components.board.tsx
@@ -1,5 +1,4 @@
 import { createBoard, Variant } from '@wixc3/react-board';
-import classNames from 'classnames';
 import { Accordion } from '~/components/accordion/accordion';
 import { ProductCard } from '~/components/product-card/product-card';
 import { QuantityInput } from '~/components/quantity-input/quantity-input';
@@ -17,46 +16,50 @@ export default createBoard({
                     <span className={styles.foundation}> | Core components</span>
                     <hr className={styles.hrSolid} />
                     <h3 className={styles.sectionTitle}>Components &amp; Elements</h3>
-                    <h4 className={styles.sectionHeader}>INPUT</h4>
                 </div>
-                <QuantityInput value={6} className={styles.quantityInput1} onChange={() => {}} />
-                <span className={styles.fontDetails}>Number Input</span>
-                <Variant name="Heading1">
-                    <hr className={styles.hrLight} />
-                    <h4 className={styles.sectionHeader}>ACCORDION</h4>
-                    <Accordion
-                        items={[
-                            {
-                                title: 'Product Info',
-                                content: 'Content',
-                            },
-                            {
-                                title: 'Return & Refund Policy',
-                                content: 'Content',
-                            },
-                            {
-                                title: 'Shipping Info ',
-                                content: 'Content',
-                            },
-                        ]}
-                        className={styles.accordion}
-                    />
-                    <hr className={styles.hrLight} />
+                <h4 className={styles.sectionHeader}>INPUT</h4>
+                <Variant name="Number Input">
+                    <QuantityInput value={6} onChange={() => {}} />
                 </Variant>
-                <p className={classNames(styles.variantName, styles.headlinesSpacing)}></p>
-                <h4 className={styles.sectionHeader}>LABELS</h4>
-                <h4>Labels missing</h4>
+                <span className={styles.variantName}>Number Input</span>
                 <hr className={styles.hrLight} />
-                <h4 className={styles.sectionHeader}>CARDS</h4>
-                <ProductCard
-                    name="Bamboo Toothbrush"
-                    imageUrl="https://static.wixstatic.com/media/c837a6_18152edaef9940ca88f446ae94b48a47~mv2.jpg/v1/fill/w_824,h_1098,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_18152edaef9940ca88f446ae94b48a47~mv2.jpg"
-                    ribbon="NEW"
-                    price="$5.5"
-                />
-                <span className={styles.fontDetails}>Product Card</span>
-                <h4>Product gallery missing</h4>
-                <span className={styles.fontDetails}>Product Card</span>
+                <h4 className={styles.sectionHeader}>ACCORDION</h4>
+                <div style={{ width: '70%' }}>
+                    <Variant name="Accordion">
+                        <Accordion
+                            items={[
+                                {
+                                    title: 'Product Info',
+                                    content: 'Description',
+                                },
+                                {
+                                    title: 'Return & Refund Policy',
+                                    content: 'Description',
+                                },
+                                {
+                                    title: 'Shipping Info ',
+                                    content: 'Description',
+                                },
+                            ]}
+                        />
+                    </Variant>
+                </div>
+                <hr className={styles.hrLight} />
+                <h4 className={styles.sectionHeader}>LABELS</h4>
+                <Variant name="Ribbon">
+                    <span className="ribbon">Sale</span>
+                </Variant>
+                <hr className={styles.hrLight} />
+                <h4 className={styles.sectionHeader}>PRODUCT CARD</h4>
+                <Variant name="Product Card">
+                    <ProductCard
+                        name="Bamboo Toothbrush"
+                        imageUrl="https://static.wixstatic.com/media/c837a6_18152edaef9940ca88f446ae94b48a47~mv2.jpg/v1/fill/w_824,h_1098,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_18152edaef9940ca88f446ae94b48a47~mv2.jpg"
+                        ribbon="NEW"
+                        price="$6"
+                        discountedPrice="$5.5"
+                    />
+                </Variant>
             </div>
         </ComponentWrapper>
     ),
