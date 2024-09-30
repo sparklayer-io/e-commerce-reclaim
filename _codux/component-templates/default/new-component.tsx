3b1b8cc9620447039b74ac79bcc43b45eb49a05a
Daniel Cohen <88962351+DanielGovrin@users.noreply.github.com>
2024-09-30 10:18:17 +0300

    feat: add a default template for creating components (#113)

---
+++ _codux/component-templates/default/new-component.tsx
@@ -0,0 +1,14 @@
+import cx from 'classnames';
+import styles from './new-component.module.scss';
+
+export interface NewComponentProps {
+    className?: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
+ */
+export const NewComponent = ({ className }: NewComponentProps) => {
+    return <div className={cx(styles.root, className)}>NewComponent</div>;
+};
