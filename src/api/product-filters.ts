aaa811907fd67241b85c715afc3a083a2583f4ba
Oleksiy Letushev <31585545+Letushev@users.noreply.github.com>
2024-10-14 18:40:16 +0200

    refactor: product filters in URL search params (#144)

--- src/api/product-filters.ts
+++ src/api/product-filters.ts
@@ -2,26 +2,17 @@ import { useCallback, useMemo } from 'react';
 import { useSearchParams } from '@remix-run/react';
 import { IProductFilters, ProductFilter } from '~/api/types';
 
-export function useProductFilters() {
+export function useAppliedProductFilters() {
     const [searchParams, setSearchParams] = useSearchParams();
 
-    const filters = useMemo(
-        () => parseProductFiltersFromUrlSearchParams(searchParams),
+    const appliedFilters = useMemo(
+        () => productFiltersFromSearchParams(searchParams),
         [searchParams],
     );
 
     const someFiltersApplied =
-        Object.values(filters).length > 0 &&
-        Object.values(filters).some((value) => value !== undefined);
-
-    const applyFilters = useCallback(
-        (filters: IProductFilters) => {
-            setSearchParams(stringifyProductFiltersToUrlSearchParams(filters), {
-                preventScrollReset: true,
-            });
-        },
-        [setSearchParams],
-    );
+        Object.values(appliedFilters).length > 0 &&
+        Object.values(appliedFilters).some((value) => value !== undefined);
 
     const clearFilters = useCallback(
         (filters: ProductFilter[]) => {
@@ -41,19 +32,16 @@ export function useProductFilters() {
     }, [clearFilters]);
 
     return {
-        filters,
+        appliedFilters,
         someFiltersApplied,
-        applyFilters,
         clearFilters,
         clearAllFilters,
     };
 }
 
-export function parseProductFiltersFromUrlSearchParams(
-    searchParams: URLSearchParams,
-): IProductFilters {
-    const minPrice = searchParams.get(ProductFilter.minPrice);
-    const maxPrice = searchParams.get(ProductFilter.maxPrice);
+export function productFiltersFromSearchParams(params: URLSearchParams): IProductFilters {
+    const minPrice = params.get(ProductFilter.minPrice);
+    const maxPrice = params.get(ProductFilter.maxPrice);
     const minPriceNumber = Number(minPrice);
     const maxPriceNumber = Number(maxPrice);
     return {
@@ -62,12 +50,12 @@ export function parseProductFiltersFromUrlSearchParams(
     };
 }
 
-export function stringifyProductFiltersToUrlSearchParams({
+export function searchParamsFromProductFilters({
     minPrice,
     maxPrice,
 }: IProductFilters): URLSearchParams {
-    const searchParams = new URLSearchParams();
-    if (minPrice !== undefined) searchParams.set(ProductFilter.minPrice, minPrice.toString());
-    if (maxPrice !== undefined) searchParams.set(ProductFilter.maxPrice, maxPrice.toString());
-    return searchParams;
+    const params = new URLSearchParams();
+    if (minPrice !== undefined) params.set(ProductFilter.minPrice, minPrice.toString());
+    if (maxPrice !== undefined) params.set(ProductFilter.maxPrice, maxPrice.toString());
+    return params;
 }
--- src/components/product-filters/product-filters.tsx
+++ src/components/product-filters/product-filters.tsx
@@ -1,48 +1,36 @@
-import { useCallback, useEffect, useState } from 'react';
-import { useNavigation } from '@remix-run/react';
+import { useCallback, useMemo } from 'react';
 import { IProductFilters } from '~/api/types';
-import { formatPrice } from '~/utils';
+import {
+    productFiltersFromSearchParams,
+    searchParamsFromProductFilters,
+} from '~/api/product-filters';
+import { formatPrice, mergeUrlSearchParams } from '~/utils';
+import { useSearchParamsOptimistic } from '~/utils/use-search-params-state';
 import { Accordion } from '../accordion/accordion';
 import { RangeSlider } from '../range-slider/range-slider';
 
 interface ProductFiltersProps {
-    appliedFilters: IProductFilters;
-    onFiltersChange: (filters: IProductFilters) => void;
     lowestPrice: number;
     highestPrice: number;
     currency: string;
 }
 
-export const ProductFilters = ({
-    appliedFilters,
-    onFiltersChange,
-    lowestPrice,
-    highestPrice,
-    currency,
-}: ProductFiltersProps) => {
-    const navigation = useNavigation();
+export const ProductFilters = ({ lowestPrice, highestPrice, currency }: ProductFiltersProps) => {
+    const [searchParams, setSearchParams] = useSearchParamsOptimistic();
 
-    // Allows updating the UI optimistically while Remix navigates to the URL
-    // with updated parameters.
-    const [filters, setFilters] = useState(appliedFilters);
+    const filters = useMemo(() => productFiltersFromSearchParams(searchParams), [searchParams]);
 
     const handleFiltersChange = (changed: Partial<IProductFilters>) => {
-        const newFilters = { ...filters, ...changed };
-        setFilters(newFilters);
-        onFiltersChange(newFilters);
+        const newParams = searchParamsFromProductFilters({ ...filters, ...changed });
+        setSearchParams((params) => mergeUrlSearchParams(params, newParams), {
+            preventScrollReset: true,
+        });
     };
 
     const formatPriceValue = useCallback(
         (price: number) => formatPrice(price, currency),
         [currency],
     );
-
-    // Synchronize filters on back/forward browser button clicks.
-    useEffect(() => {
-        if (navigation.state !== 'loading') {
-            setFilters(appliedFilters);
-        }
-    }, [navigation.state, appliedFilters]);
 
     return (
         <Accordion
