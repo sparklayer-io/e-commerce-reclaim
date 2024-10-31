f257fcc0784beb5350412296728ff37314dd2cf0
Yurii Venher <79912799+yurii-ve@users.noreply.github.com>
2024-10-31 15:10:05 +0200

    Add members area pages (#174)

---
+++ app/routes/members-area.my-account/route.tsx
@@ -0,0 +1,9 @@
+import styles from './route.module.scss';
+
+export default function MyAccountPage() {
+    return (
+        <div>
+            <div className={styles.underConstructionMessage}>This page is under construction</div>
+        </div>
+    );
+}
---
+++ app/routes/members-area.my-orders/route.tsx
@@ -0,0 +1,9 @@
+import styles from './route.module.scss';
+
+export default function MyOrdersPage() {
+    return (
+        <div>
+            <div className={styles.underConstructionMessage}>This page is under construction</div>
+        </div>
+    );
+}
