2fdce7ad15a5cf0564852b076450de1254cfdfa3
Alexey Lebedev <alisey@users.noreply.github.com>
2024-11-02 12:54:26 +0200

    refactor: spinner improvements (#190)

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -1,20 +1,29 @@
+import classNames from 'classnames';
+import { type FC } from 'react';
+
 import styles from './spinner.module.scss';
 
-interface SpinnerProps {
-    size: number;
+interface SpinnerProps extends React.SVGProps<SVGSVGElement> {
+    size: number | string;
 }
 
-export const Spinner = ({ size }: SpinnerProps) => {
-    return (
-        <svg viewBox="0 0 50 50" width={size} height={size} className={styles.spinner}>
-            <circle
-                cx="25"
-                cy="25"
-                r="20"
-                fill="none"
-                strokeWidth="1"
-                className={styles.circle}
-            ></circle>
-        </svg>
-    );
-};
+export const Spinner: FC<SpinnerProps> = ({ className, size, ...props }) => (
+    <svg
+        className={classNames(styles.spinner, className)}
+        viewBox="0 0 50 50"
+        width={size}
+        height={size}
+        {...props}
+    >
+        <circle
+            className={styles.circle}
+            cx="25"
+            cy="25"
+            r="20"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1"
+            vectorEffect="non-scaling-stroke"
+        />
+    </svg>
+);
