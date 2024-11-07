76f1f089cc689d335cc21c725c424e3654830f4e
olehrakwix <93204312+olehrakwix@users.noreply.github.com>
2024-11-07 16:17:38 +0200

    feat: add text input component styles (#201)

--- _codux/ui-kit/components/components.board.tsx
+++ _codux/ui-kit/components/components.board.tsx
@@ -18,6 +18,37 @@ export default createBoard({
             <Kit category="Core Components" title="Components & Elements">
                 <Kit.Section title="Inputs">
                     <Kit.Item>
+                        <Variant name="Input">
+                            <input className="text-input" value="text input" onChange={() => {}} />
+                        </Variant>
+                        <Kit.Description>Input</Kit.Description>
+                    </Kit.Item>
+
+                    <Kit.Item>
+                        <Variant name="Input Placeholder">
+                            <input
+                                className="text-input"
+                                placeholder="input placeholder"
+                                value=""
+                                onChange={() => {}}
+                            />
+                        </Variant>
+                        <Kit.Description>Input Placeholder</Kit.Description>
+                    </Kit.Item>
+
+                    <Kit.Item>
+                        <Variant name="Disabled Input">
+                            <input
+                                disabled
+                                className="text-input"
+                                value="disabled input"
+                                onChange={() => {}}
+                            />
+                        </Variant>
+                        <Kit.Description>Disabled Input</Kit.Description>
+                    </Kit.Item>
+
+                    <Kit.Item>
                         <Variant name="Number Input">
                             <QuantityInput value={6} onChange={() => {}} />
                         </Variant>
