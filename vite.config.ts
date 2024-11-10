83cd7ad40ad38f7ef1ae229b3d8d0e4b82c6f87f
Avi Vahl <avi.vahl@wix.com>
2024-11-10 22:39:34 +0200

    feat: enable future flags for forward compatibility (#231)

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import { netlifyPlugin } from '@netlify/remix-adapter/plugin';
 export default defineConfig({
     plugins: [
         remix({
+            future: {
+                v3_fetcherPersist: true,
+                v3_lazyRouteDiscovery: true,
+                v3_relativeSplatPath: true,
+                v3_singleFetch: true,
+                v3_throwAbortReason: true,
+            },
             ignoredRouteFiles: ['**/*.module.scss'],
         }),
         netlifyPlugin(),
