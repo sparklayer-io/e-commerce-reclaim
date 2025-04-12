9b9969e0f37f3e841fa0d1bc221a05ff13ed855e
Tom Heaton <thomas.m.heaton@gmail.com>
2025-04-12 23:09:12 +0100

    feat: SparkLayer

---
+++ src/components/spark-product-rrp/spark-product-rrp.tsx
@@ -0,0 +1,10 @@
+export interface SparkProductRRPProps {
+    parentId: string;
+}
+
+/**
+ * This component was created by SparkLayer.
+ */
+export const SparkProductRRP = ({ parentId }: SparkProductRRPProps) => {
+    return <spark-product-rrp parent-id={parentId} />;
+};
